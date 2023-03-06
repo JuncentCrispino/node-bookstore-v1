6b7f3ca1c43fc982a84468388f9dd8fd4bb7363e
Juncent Crispino <juncent@bluehive.com.ph>
2023-03-06 11:48:36 +0800

    updated appspec.yml

--- scirpts/after_install.sh
+++ scirpts/after_install.sh
@@ -1,1 +1,8 @@
-npm intall
+#!/bin/bash
+echo 'run after_install.sh: ' >> /home/ubunu/bookstore/server/deploy.log
+
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ubunu/bookstore/server/deploy.log
+cd /home/ubunu/bookstore/server >> /home/ubunu/bookstore/server/deploy.log
+
+echo 'npm install' >> /home/ubunu/bookstore/server/deploy.log 
+npm install >> /home/ubunu/bookstore/server/deploy.log
--- scirpts/application_start.sh
+++ scirpts/application_start.sh
@@ -1,1 +1,6 @@
-pm2 restart ecosytem.config.cjs --env production
+#!/bin/bash
+
+echo 'run application_start.sh: ' >> /home/ubunu/bookstore/server/deploy.log
+
+echo 'pm2 start ecosystem.config.cjs --env production' >> /home/ubunu/bookstore/server/deploy.log
+pm2 start ecosystem.config.cjs --env production >> /home/ubunu/bookstore/server/deploy.log
