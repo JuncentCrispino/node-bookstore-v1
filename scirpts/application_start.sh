f8bdac51b4ef270e17f9cc77461303aa0909bd40
Juncent Crispino <juncent@bluehive.com.ph>
2023-03-06 12:11:20 +0800

    updated appspec.yml

--- scirpts/application_start.sh
+++ scirpts/application_start.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ubunu/bookstore/server/deploy.log
-
-echo 'pm2 start ecosystem.config.cjs --env production' >> /home/ubunu/bookstore/server/deploy.log
-pm2 start ecosystem.config.cjs --env production >> /home/ubunu/bookstore/server/deploy.log
+pm2 start ecosystem.config.cjs --env production
